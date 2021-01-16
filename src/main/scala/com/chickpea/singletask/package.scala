6786affbdad5cf631c86ebbada7dba167ee7a3d1
hodongman <jhd9206@gmail.com>
2021-01-16 12:49:04 +0900

    Single Task first update

--- src/main/scala/com/chickpea/singletask/package.scala
+++ src/main/scala/com/chickpea/singletask/package.scala
@@ -9,11 +9,34 @@ import com.chickpea.util.ISO8601
 
 package object singletasks {
 
-    case class ForResponseSingleTasks(singleTasks: Seq[SingleTaskForResponse])
+    case class ForResponseSingleTasks(singleTasks: Seq[SingleTaskForResponse], singleTaskCount: Int)
+
+    case class ForResponseSingleTask(singleTask: SingleTaskForResponse)
+
+    case class SingleTaskUpdated( title: Option[String],
+                                  description: Option[String])
+
+    case class SingleTaskPosted( title: String,
+                                 description: String) {
+        def create(userId: Long): SingleTask = {
+            SingleTask(0, 
+                       title, 
+                       description, 
+                       userId, 
+                       new Timestamp((new Date).getTime), 
+                       new Timestamp((new Date).getTime), 
+                       new Timestamp((new Date).getTime), 
+                       new Timestamp((new Date).getTime))
+        }
+    }
 
     case class SingleTaskForResponse( title: String,
                                       description: String,
+                                      startDate: String,
+                                      endDate: String,
                                       createdAt: String,
-                                      updatedAt: String
-                                      /*user: Profile*/)
+                                      updatedAt: String)
+
+    case class SingleTaskRequest( limit: Option[Long],
+                                  offset: Option[Long])
 }
